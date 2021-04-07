035b19dd4a95af78f8c0a6177c5e9004bd0cd0a3
Jimmy <teohminwei@gmail.com>
2021-04-07 23:14:51 +0800

    update

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from dash.dependencies import Output, Input, State
 import pandas as pd
 import numpy as np
 import datetime
+import yfinance as yf
 import sharpe_ratio_pkg as sharpe
+from scipy import stats
 from plotly.subplots import make_subplots
 from plotly.colors import DEFAULT_PLOTLY_COLORS
 import plotly.graph_objects as go
@@ -19,9 +21,8 @@ api_key = os.environ.get("access-key")
 
 cols = DEFAULT_PLOTLY_COLORS
 
-# end_date_default = datetime.datetime.today().strftime('%Y-%m-%d')
-end_date_default = '2021-02-26'
-start_date_default = (datetime.datetime.today() - datetime.timedelta(weeks=26)).strftime('%Y-%m-%d')
+end_date_default = datetime.datetime.today().strftime('%Y-%m-%d')
+start_date_default = (datetime.datetime.today() - datetime.timedelta(weeks=12)).strftime('%Y-%m-%d')
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
@@ -30,6 +31,7 @@ server = app.server
 
 app.title = 'Portfolio Optimizer'
 
+debug_mode = False
 num_options = 5
 
 header_card = html.Div(
@@ -44,21 +46,39 @@ header_card = html.Div(
     className='header'
 )
 
-list_stocks = ['AMD', 'SE', 'RTX', 'MSFT', 'TSLA', 'AMZN', 'BABA', 'RIO', 'GOOGL', 'ACN', 'APA', 'KO']
-
-stock_data_master = pd.read_csv('stock_data.csv')
-stock_data_master['date'] = pd.to_datetime(stock_data_master['date'])
+current_portfolio = {
+    'TSLA': 1.4,
+    'AMC': 1,
+    'GME': 2,
+    'MSFT': 1,
+    'AAPL': 5
+}
 
 
 def query_stock(ticker, date_from, date_to):
-    stock_data = stock_data_master[
-        (stock_data_master['symbol'] == ticker) &
-        (stock_data_master['date'] >= date_from) &
-        (stock_data_master['date'] < date_to)
-    ].copy()
+    stock_data = yf.download(
+        tickers=ticker,
+        group_by='ticker',
+        start=date_from,
+        end=date_to,
+        threads=True
+    )
 
-    stock_data.sort_values('date', inplace=True)
-    stock_data.loc[:, 'final_volume'] = stock_data['adj_volume'].combine_first(stock_data['volume'])
+    if stock_data.shape[0] == 0:
+        return pd.DataFrame()
+    elif ticker[0] == '^':
+        stock_data['symbol'] = ticker + '.INDX'
+        stock_data['exchange'] = 'INDX'
+    else:
+        stock_data['symbol'] = ticker
+        stock_data['exchange'] = 'EQUITY'
+
+    stock_data.reset_index(inplace=True)
+    stock_data.sort_values('Date', inplace=True)
+
+    stock_data.rename(columns={'Date': 'date',
+                               'Adj Close': 'adj_close',
+                               'Volume': 'final_volume'}, inplace=True)
 
     return stock_data
 
@@ -66,31 +86,27 @@ def query_stock(ticker, date_from, date_to):
 def generate_options_card(num_options):
     options_card = []
 
-    selected_stocks = random.sample(list_stocks, num_options)
     for i in range(num_options):
         # handle titles
         if i == 0:
             stock_card = [
                 html.Div(children='Stock Ticker', className='menu-title'),
-                dcc.Dropdown(
+                dcc.Input(
                     id=f'stock_name_input_{i}',
-                    options=[
-                        {'label': ticker, 'value': ticker}
-                        for ticker in np.sort(stock_data_master['symbol'].unique())
-                    ],
+                    type='search',
                     placeholder='Input Ticker here',
-                    value=selected_stocks[i],
-                    className='dropdown',
-                    clearable=False
+                    value=list(current_portfolio)[i],
+                    className='input',
+                    multiple=True
                 )
             ]
             value_input_card = [
-                html.Div(children='Amount Invested', className='menu-title'),
+                html.Div(children='Units Bought', className='menu-title'),
                 dcc.Input(
                     id=f'initial_amt_{i}',
                     type='number',
-                    placeholder=1000,
-                    value=random.randint(0, 1000),
+                    placeholder=1,
+                    value=list(current_portfolio.values())[i],
                     className='input'
                 )
             ]
@@ -98,7 +114,7 @@ def generate_options_card(num_options):
                 html.Div(children='Date Range', className='menu-title'),
                 dcc.DatePickerRange(
                     id=f'date_range_{i}',
-                    min_date_allowed=datetime.date(2017, 3, 18),
+                    min_date_allowed=datetime.date(2010, 1, 1),
                     max_date_allowed=datetime.datetime.strptime(end_date_default, '%Y-%m-%d'),
                     start_date=datetime.datetime.strptime(start_date_default, '%Y-%m-%d'),
                     end_date=datetime.datetime.strptime(end_date_default, '%Y-%m-%d'),
@@ -108,31 +124,28 @@ def generate_options_card(num_options):
             ]
         else:
             stock_card = [
-                dcc.Dropdown(
+                dcc.Input(
                     id=f'stock_name_input_{i}',
-                    options=[
-                        {'label': ticker, 'value': ticker}
-                        for ticker in np.sort(stock_data_master['symbol'].unique())
-                    ],
+                    type='search',
                     placeholder='Input Ticker here',
-                    value=selected_stocks[i],
-                    className='dropdown',
-                    clearable=False
+                    value=list(current_portfolio)[i],
+                    className='input',
+                    multiple=True
                 )
             ]
             value_input_card = [
                 dcc.Input(
                     id=f'initial_amt_{i}',
                     type='number',
-                    placeholder=1000,
-                    value=random.randint(0, 1000),
+                    placeholder=1,
+                    value=list(current_portfolio.values())[i],
                     className='input'
                 )
             ]
             date_range_card = [
                 dcc.DatePickerRange(
                     id=f'date_range_{i}',
-                    min_date_allowed=datetime.date(2017, 3, 29),
+                    min_date_allowed=datetime.date(2010, 1, 1),
                     max_date_allowed=datetime.datetime.strptime(end_date_default, '%Y-%m-%d'),
                     start_date=datetime.datetime.strptime(start_date_default, '%Y-%m-%d'),
                     end_date=datetime.datetime.strptime(end_date_default, '%Y-%m-%d'),
@@ -182,6 +195,16 @@ graphs_card = html.Div(
                   ),
                   className='card',
               ),
+              html.Div(children=[
+                  html.Div(children='Alpha and Beta of Portfolio to Indexes', className='menu-card-title-alpha-beta'),
+                  html.Div(id='alpha-beta-container',
+                           children=[
+                                  html.Div(id='greeks-index-1', className='card-for-alpha-beta'),
+                                  html.Div(id='greeks-index-2', className='card-for-alpha-beta'),
+                                  html.Div(id='greeks-index-3', className='card-for-alpha-beta')
+                            ], style={'columnCount': 3})
+              ], className='container-alpha-beta'
+              ),
               html.Div(
                   children=[
                       html.Div(children='Select type of graph', className='menu-card-title'),
@@ -192,9 +215,9 @@ graphs_card = html.Div(
                               for x in ['Stocks Raw Price vs Index',
                                         'Stocks Normalized vs Index',
                                         'Portfolio Normalized vs Index']],
-                          value='Stocks Normalized vs Index',
+                          value='Portfolio Normalized vs Index',
                           clearable=False,
-                          className='input-special'
+                          className='dropdown'
                       ),
                       dcc.Graph(
                           id='all-stock-chart', config={'displayModeBar': False},
@@ -261,14 +284,14 @@ def query_data(*args):
 
     ticker_index = [index_num for index_num in range(len(args)) if (index_num - 1) % 4 == 0]
     stock_tickers = [args[index_num] for index_num in ticker_index]
-    value_invested = [args[index_num + 1] for index_num in ticker_index]
+    num_stocks = [args[index_num + 1] for index_num in ticker_index]
     start_date = [args[index_num + 2] for index_num in ticker_index]
     end_date = [args[index_num + 3] for index_num in ticker_index]
 
     interested_df = pd.DataFrame(
         {
             'stock_ticker': stock_tickers,
-            'value_invested': value_invested,
+            'num_stocks': num_stocks,
             'start_date': start_date,
             'end_date': end_date
         }
@@ -283,6 +306,9 @@ def query_data(*args):
     interested_df['start_date'] = interested_df['start_date'].dt.strftime('%Y-%m-%d')
     interested_df['end_date'] = interested_df['end_date'].dt.strftime('%Y-%m-%d')
 
+    if debug_mode:
+        interested_df = pd.read_csv('interested_df.csv')
+
     print(f'Searched {n_clicks} time(s) at {datetime.datetime.now()}')
 
     # query data
@@ -291,13 +317,16 @@ def query_data(*args):
         If ticker change, only re-query the changed ticker,
         If date range change, check whether new date range is within the old date range
     '''
-    result_list = [query_stock(row[0], row[1], row[2]) for row in zip(interested_df['stock_ticker'],
-                                                                        interested_df['start_date'],
-                                                                        interested_df['end_date'])]
-    stock_data = pd.concat(result_list)
+    if debug_mode:
+        stock_data = pd.read_csv('stock_data.csv')
+    else:
+        result_list = [query_stock(row[0], row[1], row[2]) for row in zip(interested_df['stock_ticker'],
+                                                                          interested_df['start_date'],
+                                                                          interested_df['end_date'])]
+        stock_data = pd.concat(result_list)
 
     # query indexes (S&P 500 & DJI)
-    indexes_list = ['GSPC.INDX', 'IXIC.INDX', 'DJI.INDX']
+    indexes_list = ['^GSPC', '^IXIC', '^DJI']
 
     for market_index in indexes_list:
         stock_data = stock_data.append(query_stock(market_index, earliest_start_date, latest_end_date))
@@ -310,21 +339,22 @@ def query_data(*args):
 
     if len(difference_string) > 0:
         alert = dbc.Alert(f'The following ticker(s): {difference_string} does not exist. '
-                          'Please search on https://marketstack.com/search for the tickers supported',
+                          'Please search on Yahoo Finance for the tickers supported',
                           color='danger', dismissable=False, className='alert-bar')
     else:
         alert = None
 
     first_row = stock_data.groupby(['symbol']).first()[['date', 'adj_close']]
-    initial_data = interested_df[['stock_ticker', 'value_invested',
+    initial_data = interested_df[['stock_ticker', 'num_stocks',
                                   'start_date', 'end_date']].merge(first_row,
                                                                    left_on='stock_ticker',
                                                                    right_on=first_row.index)
-    initial_data['unit_bought'] = initial_data['value_invested'] / initial_data['adj_close']
+    initial_data['unit_bought'] = initial_data['num_stocks']
+    initial_data['value_invested'] = initial_data['unit_bought'] * initial_data['adj_close']
     initial_data = initial_data[['stock_ticker', 'date', 'end_date', 'value_invested', 'unit_bought']]
     initial_data.rename(columns={'date': 'start_date'}, inplace=True)
 
-    combined_data = stock_data[['symbol', 'name', 'exchange', 'adj_close', 'date']].merge(initial_data,
+    combined_data = stock_data[['symbol', 'exchange', 'adj_close', 'date']].merge(initial_data,
                                                                                           left_on='symbol',
                                                                                           right_on='stock_ticker',
                                                                                           how='left')
@@ -445,6 +475,9 @@ def update_charts(jsonified_cleaned_data):
 @app.callback(
     [
         Output('all-stock-chart', 'figure'),
+        Output('greeks-index-1', 'children'),
+        Output('greeks-index-2', 'children'),
+        Output('greeks-index-3', 'children')
     ],
     [
         Input('query_df', 'children'),
@@ -453,9 +486,15 @@ def update_charts(jsonified_cleaned_data):
 )
 def update_performance_all(query_df, type_of_graph):
     combined_data = pd.read_json(query_df[0], orient='split')
-    combined_data['symbol'] = np.where(combined_data['exchange'] == 'INDX',
-                                       combined_data['name'],
-                                       combined_data['symbol'])
+
+    non_index_stocks = combined_data[combined_data['exchange'] != 'INDX']
+    grouped_portfolio = non_index_stocks.groupby(['date'])['total_value'].sum().to_frame().reset_index()
+    grouped_portfolio['symbol'] = 'PORTFOLIO'
+    grouped_portfolio = grouped_portfolio[['date', 'symbol', 'total_value']]
+
+    index_stocks = combined_data[combined_data['exchange'] == 'INDX'].copy()
+    index_stocks.loc[:, 'total_value'] = index_stocks.loc[:, 'adj_close']
+    index_stocks = index_stocks[['date', 'symbol', 'total_value']]
 
     overall_plot = go.Figure()
 
@@ -486,21 +525,12 @@ def update_performance_all(query_df, type_of_graph):
                 overall_plot.add_trace(
                     go.Scatter(x=temp_stock['date'], y=1000 * temp_stock['adj_close'] / initial_price,
                                name=stock_name, hovertemplate='%{y:.0f}',
-                               line=dict(color=cols[stock_num], dash=dash_value)
+                               line=dict(color=cols[stock_num],  dash=dash_value),
                                )
                 )
 
                 overall_plot.update_yaxes(title_text='Price Normalized')
     elif type_of_graph == 'Portfolio Normalized vs Index':
-        non_index_stocks = combined_data[combined_data['exchange'] != 'INDX']
-        grouped_portfolio = non_index_stocks.groupby(['date'])['total_value'].sum().to_frame().reset_index()
-        grouped_portfolio['symbol'] = 'PORTFOLIO'
-        grouped_portfolio = grouped_portfolio[['date', 'symbol', 'total_value']]
-
-        index_stocks = combined_data[combined_data['exchange'] == 'INDX'].copy()
-        index_stocks.loc[:, 'total_value'] = index_stocks.loc[:, 'adj_close']
-        index_stocks = index_stocks[['date', 'symbol', 'total_value']]
-
         master_data = pd.concat([grouped_portfolio, index_stocks])
 
         num_stocks = master_data['symbol'].nunique()
@@ -532,7 +562,26 @@ def update_performance_all(query_df, type_of_graph):
                                            xanchor='right', x=1),
                                title_text='All stock(s) historical prices compared to indexes')
 
-    return [overall_plot]
+    # run alpha-beta analysis
+    daily_returns_overall = grouped_portfolio['total_value'].pct_change()[1:]
+
+    index_table = pd.pivot_table(data=index_stocks, index='date', columns='symbol', values='total_value', aggfunc='sum')
+    index_returns = index_table.pct_change()[1:]
+
+    output_perf_list = [overall_plot]
+
+    for index_stock_name in index_stocks['symbol'].unique():
+        beta, alpha, r_value = stats.linregress(index_returns[index_stock_name].values,
+                                                daily_returns_overall.values)[:3]
+        output_perf_list.append(dcc.Markdown(f"""
+                                    **{index_stock_name}**
+
+                                    Alpha α: {round(alpha * 100, 3)}%  
+                                    Beta β: {round(beta, 5)}  
+                                    R-squared: {round(r_value**2, 2)}
+                                """))
+
+    return output_perf_list
 
 
 @app.callback(
